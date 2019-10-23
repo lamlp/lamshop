f574f589fa484452446f1af85bf00650123a2185
nnlam94 <nnlam94@gmail.com>
2019-10-23 20:10:59 +0700

    The second commit

---
+++ LamShop.Model/Models/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamShop.Model.Models
+{
+    [Table("Menu")]
+    public class Menu
+    {
+        [Key]
+        public int ID { set; get; }
+        [Required]
+        public string Name { set; get; }
+        [Required]
+        public string URL { set; get; }
+        public int? DisplayOrder { set; get; }
+        [Required]
+        public int GroupID { set; get; }
+
+        [ForeignKey("GroupID")]
+        public virtual MenuGroup MenuGroup { set; get; }
+
+        public string Target { set; get; }
+        [Required]
+        public bool Status { set; get; }
+    }
+}
