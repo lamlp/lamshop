3708c42053033527ce8c511cc5fc817cdb56ec64
nnlam94 <nnlam94@gmail.com>
2019-10-26 21:52:37 +0700

    The five Commit - Complete Migration

---
+++ LamShop.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using LamShop.Data.Infrastructure;
+using LamShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LamShop.Data.Repositories
+{
+    public interface IProductCategoryRepository
+    {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+
+    public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
+    {
+        public ProductCategoryRepository(DbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
+        {
+            return this.DbContext.ProductCategories.Where(x => x.Alias == alias);
+        }
+    }
+}
